9db946ea173f129d0962370e8c98a9e5cf2abfdc
0x1abin <270995079@qq.com>
2017-09-23 14:09:31 +0800

    remove the now ignored directory

---
+++ examples/Advanced/Lora/LoRaReceiver/LoRaReceiver.ino
@@ -0,0 +1,45 @@
+//nedd to download library https://github.com/sandeepmistry/arduino-LoRa.git
+
+#include <M5Stack.h>
+#include <SPI.h>
+#include <LoRa.h>
+
+void setup() {
+  
+  M5.begin();
+
+  // override the default CS, reset, and IRQ pins (optional)
+  LoRa.setPins(5, 26, 36); // set CS, reset, IRQ pin
+
+  Serial.println("LoRa Receiver");
+  M5.Lcd.println("LoRa Receiver");
+
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    M5.Lcd.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet: \"");
+    M5.Lcd.print("Received packet: \"");
+
+    // read packet
+    while (LoRa.available()) {
+      char ch = (char)LoRa.read();
+      Serial.print(ch);
+      M5.Lcd.print(ch);
+    }
+
+    // print RSSI of packet
+    Serial.print("\" with RSSI ");
+    Serial.println(LoRa.packetRssi());
+    M5.Lcd.print("\" with RSSI ");
+    M5.Lcd.println(LoRa.packetRssi());
+  }
+}
