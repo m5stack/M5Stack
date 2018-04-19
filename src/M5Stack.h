59b5a32445dde86dbe82993dfe991d59095547d4
0x1abin <270995079@qq.com>
2018-04-19 14:27:51 +0800

    added git tag version

--- src/M5Stack.h
+++ src/M5Stack.h
@@ -84,16 +84,13 @@
 #if defined(ESP32)
 
 // #define MPU9250_INSDE
-
 #include <Arduino.h>
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <WiFiMulti.h>
 #include <Wire.h>
 #include <SPI.h>
 #include "FS.h"
 #include "SD.h"
 
+#include "gitTagVersion.h"
 #include "utility/Display.h"
 #include "utility/Config.h"
 #include "utility/Button.h"
---
+++ src/gitTagVersion.h
@@ -0,0 +1,1 @@
+#define M5_LIB_VERSION F("0.1.8-dirty")
