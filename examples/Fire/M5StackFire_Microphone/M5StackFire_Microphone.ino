57f4f79f2faf78b974e60c15b8d26bb1909d4b15
ChrisMicro <christoph@roboterclub-freiburg.de>
2018-09-16 09:00:34 +0200

    M5StackFire examples added

---
+++ examples/Fire/M5StackFire_Microphone/M5StackFire_Microphone.ino
@@ -0,0 +1,25 @@
+/*
+ microphone test
+
+   hardwware:  M5StackFire 
+
+ please use the serial plotter in the Arduino IDE to show the signal
+ change the baudrate of the plotter to 115200
+ 
+ September 2018, ChrisMicro
+*/
+
+#define M5STACKFIRE_MICROPHONE_PIN 34
+
+void setup() 
+{
+  Serial.begin(115200);
+}
+
+void loop() 
+{
+  int micValue = analogRead(M5STACKFIRE_MICROPHONE_PIN);
+
+  Serial.println(micValue);
+  delay(1);       
+}
