8a12d29a65b17a15f39a46e29214ef0d198415bd
tobozo <tobozo@users.noreply.github.com>
2019-12-16 12:06:25 +0100

    Revert "psram for sprites is now optional (default enabled)"
    This reverts commit 096de8507564766bb3b81eca8f1ffd4c92680b74.

--- src/utility/Sprite.cpp
+++ src/utility/Sprite.cpp
@@ -107,7 +107,7 @@ void* TFT_eSprite::callocSprite(int16_t w, int16_t h, uint8_t frames)
   {
 
 #if defined (ESP32) && defined (CONFIG_SPIRAM_SUPPORT)
-    if ( psramFound() && _usePsram ) ptr8 = ( uint8_t*) ps_calloc(w * h + 1, sizeof(uint16_t));
+    if ( psramFound() ) ptr8 = ( uint8_t*) ps_calloc(w * h + 1, sizeof(uint16_t));
     else
 #endif
     ptr8 = ( uint8_t*) calloc(w * h + 1, sizeof(uint16_t));
@@ -116,7 +116,7 @@ void* TFT_eSprite::callocSprite(int16_t w, int16_t h, uint8_t frames)
   else if (_bpp == 8)
   {
 #if defined (ESP32) && defined (CONFIG_SPIRAM_SUPPORT)
-    if ( psramFound() && _usePsram) ptr8 = ( uint8_t*) ps_calloc(w * h + 1, sizeof(uint8_t));
+    if ( psramFound() ) ptr8 = ( uint8_t*) ps_calloc(w * h + 1, sizeof(uint8_t));
     else
 #endif
     ptr8 = ( uint8_t*) calloc(w * h + 1, sizeof(uint8_t));
@@ -135,7 +135,7 @@ void* TFT_eSprite::callocSprite(int16_t w, int16_t h, uint8_t frames)
     if (frames > 2) frames = 2; // Currently restricted to 2 frame buffers
     if (frames < 1) frames = 1;
 #if defined (ESP32) && defined (CONFIG_SPIRAM_SUPPORT)
-    if ( psramFound() && _usePsram) ptr8 = ( uint8_t*) ps_calloc(frames * (w>>3) * h + frames, sizeof(uint8_t));
+    if ( psramFound() ) ptr8 = ( uint8_t*) ps_calloc(frames * (w>>3) * h + frames, sizeof(uint8_t));
     else
 #endif
     ptr8 = ( uint8_t*) calloc(frames * (w>>3) * h + frames, sizeof(uint8_t));
@@ -225,17 +225,6 @@ void TFT_eSprite::deleteSprite(void)
 
   _created = false;
 }
-
-
-
-/***************************************************************************************
-** Function name:           setPsram
-** Description:             Disable psRam (only if applicable)
-*************************************************************************************x*/
-void TFT_eSprite::setPsram( bool enabled ) {
-  _usePsram = enabled;
-}
-
 
 
 /***************************************************************************************
