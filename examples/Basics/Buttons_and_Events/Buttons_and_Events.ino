dc8710140a86f4d37b32c06af8b53e334eb277a9
Rop Gonggrijp <rop@gonggri.jp>
2020-10-01 17:10:46 +0200

    Added example

---
+++ examples/Basics/Buttons_and_Events/Buttons_and_Events.ino
@@ -0,0 +1,21 @@
+#include <M5Stack.h>
+
+void setup() {
+  M5.begin();
+  M5.BtnA.setLabel("Test");
+  M5.BtnB.setLabel("Wow !");
+  M5.BtnC.setLabel("Amazing !");
+  M5.BtnA.off = M5.BtnB.off = M5.BtnC.off = {BLUE, WHITE, NODRAW};
+  M5.BtnA.on = M5.BtnB.on = M5.BtnC.on = {RED, WHITE, NODRAW};
+  M5.Events.addHandler(eventDisplay);
+  M5.Buttons.draw();
+}
+
+void loop() {
+	M5.update();
+}
+
+void eventDisplay(Event& e) {
+  Serial.printf("\n%-12s %-18s", e.typeName(), e.objName());
+  if (e.type == E_RELEASE || e.type == E_PRESSED) Serial.printf("%5d ms", e.duration);
+}
