30baff1a058fdae68d9629bc84ff39551ce33533
Tinyu <Tinyu.Zhao@gmail.com>
2021-08-05 17:02:54 +0800

    add some Ex&explain

--- examples/Advanced/Serial/Serial.ino
+++ examples/Advanced/Serial/Serial.ino
@@ -1,27 +1,26 @@
-#include <M5Stack.h>
+/*
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with M5Core sample source code
+*                          配套  M5Core 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/core/gray
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/core/gray
+*
+* describe：Serial.  串口发送接收
+* date：2021/7/26
+*******************************************************************************
+*/
 
+#include <M5Core2.h>
 
 void setup() {
-
-  M5.begin();
-  M5.Power.begin();
-  Serial.begin(115200);
-
-  // Serial2.begin(unsigned long baud, uint32_t config, int8_t rxPin, int8_t txPin, bool invert)
-  Serial2.begin(115200, SERIAL_8N1, 16, 17);
-  pinMode(5, OUTPUT);
-  digitalWrite(5, 1);
+  M5.begin(); //Init M5Stack(Serial initialization is also included, the baud rate is 115200).  初始化M5Stack(串口初始化也包含在内,波特率为115200)
+  M5.Power.begin(); //Init power  初始化电源模块
 }
 
 void loop() {
-
- if(Serial.available()) {
-   int ch = Serial.read();
-   Serial2.write(ch);
- }
-
- if(Serial2.available()) {
-   int ch = Serial2.read();
-   Serial.write(ch);
- }
+  if(Serial.available()) {  //If serial port is available.  如果串口可用
+    int ch = Serial.read(); //Store the data read by serial port to ch.  将串口读取到到数据存储至ch中
+    Serial.write(ch); //Send data from ch to serial port.  将ch中的数据发送到串口
+  }
 }
