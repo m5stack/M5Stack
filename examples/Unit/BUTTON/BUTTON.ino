720de488cb1c4997f9acb3c06c1b6ae9daa52b10
Tinyu <Tinyu.Zhao@gmail.com>
2021-08-10 09:21:41 +0800

    button pin

--- examples/Unit/BUTTON/BUTTON.ino
+++ examples/Unit/BUTTON/BUTTON.ino
@@ -9,9 +9,10 @@
 * describe：Button.  按键
 * date：2021/8/9
 *******************************************************************************
-  Please connect to Port A,Read the button status of BUTTON Unit and display it on the screen
-
-  请连接端口A,读取按键的状态并在显示屏上显示
+  Please connect to Port A(36),Read the button status of BUTTON Unit and display it on the screen
+  请连接端口A(36),读取按键的状态并在显示屏上显示
+  if you don't have M5GO BOTTOM, you need change the pinMode and the digitalRead to 22, But you will not be able to use any I2C operations.
+  如果你没有M5GO BOTTOM，你需要改变pinMode和digitalRead到22,但是你将不能使用任何I2C操作.
 */
 #include <M5Stack.h>
 
@@ -21,7 +22,7 @@ int cur_value = 0;
 void setup() {
   M5.begin(); //Init M5Stack.  初始化M5Stack
   M5.Power.begin(); //Init power  初始化电源模块
-  pinMode(22, INPUT); //set pin mode to input.设置引脚模式为输入模式
+  pinMode(36, INPUT); //set pin mode to input.设置引脚模式为输入模式
   M5.Lcd.setTextColor(YELLOW);  //Set the font color to yellow.  设置字体颜色为黄色
   M5.Lcd.setTextSize(2);  //Setting the Font size.  设置字号大小
   M5.Lcd.setCursor(80, 0);  //Set the cursor position to (80,0).  将光标位置设置为(80,0)
@@ -30,7 +31,7 @@ void setup() {
 }
 
 void loop() {
-  cur_value = digitalRead(22); // read the value of BUTTON.  读取22号引脚的值
+  cur_value = digitalRead(36); // read the value of BUTTON.  读取22号引脚的值
   M5.Lcd.setCursor(80,25); M5.Lcd.print("Button");
   M5.Lcd.setCursor(0,45); M5.Lcd.print("Value: ");
   M5.Lcd.setCursor(0,85); M5.Lcd.print("State: ");
