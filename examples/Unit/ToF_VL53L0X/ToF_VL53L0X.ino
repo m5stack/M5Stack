bc3cce2bad9b3f657a2f4b15b0ca4b5348b6bc7f
Tinyu <Tinyu.Zhao@gmail.com>
2021-08-16 11:24:39 +0800

    dac,tof

--- examples/Unit/ToF_VL53L0X/ToF_VL53L0X.ino
+++ examples/Unit/ToF_VL53L0X/ToF_VL53L0X.ino
@@ -1,9 +1,19 @@
 /*
-    Description: Use ToF Unit to detect distance and display distance data on the screen in real time.
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with M5Core sample source code
+*                          配套  M5Core 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/core/gray
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/core/gray
+*
+* describe：ToF.  激光测距
+* date：2021/8/16
+*******************************************************************************
+  Please connect to Port A(22、21),Use ToF Unit to detect distance and display distance data on the screen in real time.
+  请连接端口A(22、21),使用ToF Unit检测距离，并在屏幕上实时显示距离数据。
 */
 
 #include <M5Stack.h>
-#include <Wire.h>
 
 #define VL53L0X_REG_IDENTIFICATION_MODEL_ID         0xc0
 #define VL53L0X_REG_IDENTIFICATION_REVISION_ID      0xc2
@@ -12,84 +22,9 @@
 #define VL53L0X_REG_SYSRANGE_START                  0x00
 #define VL53L0X_REG_RESULT_INTERRUPT_STATUS         0x13
 #define VL53L0X_REG_RESULT_RANGE_STATUS             0x14
-#define address 0x29
+#define address 0x29  //I2C address
 
 byte gbuf[16];
-
-void setup() {
-  // put your setup code here, to run once:
-  Wire.begin();        // join i2c bus (address optional for master)
-  Serial.begin(115200);  // start serial for output
-  Serial.println("VLX53LOX test started.");
-
-
-  //---osmar
-  M5.begin();
-  M5.Power.begin();
-  M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setTextColor(YELLOW);
-  M5.Lcd.setCursor(50, 0, 4);
-  M5.Lcd.println(("VLX53LOX Example"));
-  M5.Lcd.setTextColor(WHITE);
-  //---osmar
-}
-
-void loop() {
-  Serial.println("----- START TEST ----");
-  test();
-  Serial.println("----- END TEST ----");
-  Serial.println("");
-  delay(500);
-}
-
-void test() {
-  byte val1 = read_byte_data_at(VL53L0X_REG_IDENTIFICATION_REVISION_ID);
-  Serial.print("Revision ID: "); Serial.println(val1);
-
-  val1 = read_byte_data_at(VL53L0X_REG_IDENTIFICATION_MODEL_ID);
-  Serial.print("Device ID: "); Serial.println(val1);
-
-  val1 = read_byte_data_at(VL53L0X_REG_PRE_RANGE_CONFIG_VCSEL_PERIOD);
-  Serial.print("PRE_RANGE_CONFIG_VCSEL_PERIOD="); Serial.println(val1); 
-  Serial.print(" decode: "); Serial.println(VL53L0X_decode_vcsel_period(val1));
-
-  val1 = read_byte_data_at(VL53L0X_REG_FINAL_RANGE_CONFIG_VCSEL_PERIOD);
-  Serial.print("FINAL_RANGE_CONFIG_VCSEL_PERIOD="); Serial.println(val1);
-  Serial.print(" decode: "); Serial.println(VL53L0X_decode_vcsel_period(val1));
-
-  write_byte_data_at(VL53L0X_REG_SYSRANGE_START, 0x01);
-
-  byte val = 0;
-  int cnt = 0;
-  while (cnt < 100) { // 1 second waiting time max
-    delay(10);
-    val = read_byte_data_at(VL53L0X_REG_RESULT_RANGE_STATUS);
-    if (val & 0x01) break;
-    cnt++;
-  }
-  if (val & 0x01) Serial.println("ready"); else Serial.println("not ready");
-
-  read_block_data_at(0x14, 12);
-  uint16_t acnt = makeuint16(gbuf[7], gbuf[6]);
-  uint16_t scnt = makeuint16(gbuf[9], gbuf[8]);
-  uint16_t dist = makeuint16(gbuf[11], gbuf[10]);
-  byte DeviceRangeStatusInternal = ((gbuf[0] & 0x78) >> 3);
-
-  Serial.print("ambient count: "); Serial.println(acnt);
-  Serial.print("signal count: ");  Serial.println(scnt);
-  Serial.print("distance ");       Serial.println(dist);
-  Serial.print("status: ");        Serial.println(DeviceRangeStatusInternal);
-  
-  // M5.Lcd.fillScreen(BLACK);
-  // M5.Lcd.setTextColor(YELLOW);
-  // M5.Lcd.setCursor(50, 0, 4);
-  // M5.Lcd.println(("GPS Raw Example"));
-  M5.Lcd.fillRect(0, 80, 319, 239, BLACK);
-  M5.Lcd.setTextColor(WHITE);
-  M5.Lcd.setCursor(100, 100, 4);
-  M5.Lcd.printf("D:%d ",dist);
-  
-}
 
 uint16_t bswap(byte b[]) {
   // Big Endian unsigned short to little endian unsigned short
@@ -168,4 +103,46 @@ uint16_t VL53L0X_decode_vcsel_period(short vcsel_period_reg) {
   // period in PLL clocks
   uint16_t vcsel_period_pclks = (vcsel_period_reg + 1) << 1;
   return vcsel_period_pclks;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(115200);  // start serial for output
+  Serial.println("VLX53LOX test started.");
+
+  //---osmar
+  M5.begin();
+  M5.Power.begin();
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(50, 0, 4);
+  M5.Lcd.println(("VLX53LOX Example"));
+  //---osmar
+}
+
+void loop() {
+  write_byte_data_at(VL53L0X_REG_SYSRANGE_START, 0x01);
+
+  byte val = 0;
+  int cnt = 0;
+  while (cnt < 100) { // 1 second waiting time max
+    delay(10);
+    val = read_byte_data_at(VL53L0X_REG_RESULT_RANGE_STATUS);
+    if (val & 0x01) break;
+    cnt++;
+  }
+  if (val & 0x01) Serial.println("ready"); else Serial.println("not ready");
+
+  read_block_data_at(0x14, 12);
+  uint16_t acnt = makeuint16(gbuf[7], gbuf[6]);
+  uint16_t scnt = makeuint16(gbuf[9], gbuf[8]);
+  uint16_t dist = makeuint16(gbuf[11], gbuf[10]);
+  byte DeviceRangeStatusInternal = ((gbuf[0] & 0x78) >> 3);
+  M5.Lcd.fillRect(0, 35, 319, 239, BLACK);
+  M5.Lcd.setCursor(0, 35, 4);
+  M5.Lcd.print("ambient count: "); M5.Lcd.println(acnt);
+  M5.Lcd.print("signal count: ");  M5.Lcd.println(scnt);
+  M5.Lcd.print("distance: ");       M5.Lcd.println(dist);
+  M5.Lcd.print("status: ");        M5.Lcd.println(DeviceRangeStatusInternal);
+  delay(1000);
 }
