1d5e43aefe3e7cd5d55d96edb63c2ec4fa59b317
Tinyu <Tinyu.Zhao@gmail.com>
2021-08-16 12:18:12 +0800

    op,relay

--- examples/Unit/RELAY/RELAY.ino
+++ examples/Unit/RELAY/RELAY.ino
@@ -1,24 +1,38 @@
 /*
-    Description: Use RELAY to switch on and off the circuit.
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with M5Core sample source code
+*                          配套  M5Core 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/core/gray
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/core/gray
+*
+* describe：Relay.  继电器
+* date：2021/8/16
+*******************************************************************************
+  Please connect to Port B(26),Use RELAY to switch on and off the circuit.
+  请连接端口B(26),使用继电器开关电路。
 */
+
 #include <M5Stack.h>
 
 void setup() {
-  M5.begin();
-  M5.Power.begin();
-  M5.Lcd.clear(BLACK);
-  M5.Lcd.setTextFont(4);
-  M5.Lcd.setTextColor(YELLOW, BLACK);
-  M5.Lcd.setCursor(50, 0, 4);
+  M5.begin(); //Init M5Stack.  初始化M5Stack
+  M5.Power.begin(); //Init power  初始化电源模块
+  M5.lcd.setTextSize(2);  //Set the text size to 2.  设置文字大小为2
+  M5.Lcd.setCursor(50, 0);
   M5.Lcd.println(("Relay Example"));
-  //disable the speak noise
-  dacWrite(25, 0);
+  dacWrite(25, 0);  //disable the speak noise
   pinMode(26, OUTPUT);
 }
 
 void loop(void) {
+  M5.Lcd.setCursor(100, 40);
+  M5.Lcd.print("ON");
   digitalWrite(26, HIGH);
-  delay(500);
+  delay(1000);
+  M5.Lcd.fillRect(100,40,60,50,BLACK);
+  M5.Lcd.print("OFF");
   digitalWrite(26, LOW);
-  delay(500);
+  delay(1000);
+  M5.Lcd.fillRect(100,40,60,50,BLACK);
 }
