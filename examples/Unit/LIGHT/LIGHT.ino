3fd80b97de3cccbafb67ac1cd6c3daad92de244b
Tinyu <Tinyu.Zhao@gmail.com>
2021-08-18 11:26:13 +0800

    lcd、light

--- examples/Unit/LIGHT/LIGHT.ino
+++ examples/Unit/LIGHT/LIGHT.ino
@@ -1,40 +1,37 @@
 /*
-    Description: Read the analog quantity and digital quantity input by LIGHT Unit, and convert the analog quantity into 12-bit data for display
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with M5Core sample source code
+*                          配套  M5Core 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/core/gray
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/core/gray
+*
+* describe: Light.  环境光传感器
+* date：2021/8/18
+*******************************************************************************
+  Please connect to Port B,Use the Light Unit screen to display the current ambient lighting value
+  请连接端口 B ,使用Light Unit 屏幕显示当前环境光照值。
 */
+
 #include <M5Stack.h>
 
 void setup() {
-  M5.begin();
-  M5.Power.begin();
-  //disable the speak noise
-  dacWrite(25, 0);
-
-  M5.Lcd.setTextColor(YELLOW);
-  M5.Lcd.setTextSize(2);
-  M5.Lcd.setCursor(65, 50);
-  M5.Lcd.printf("UNIT_LIGHT EXAMPLE\n");
-  pinMode(26, INPUT);
-  M5.Lcd.setCursor(85, 80);
-  M5.Lcd.print("GPIO36:");
-  M5.Lcd.setCursor(85, 100);
-  M5.Lcd.print("GPIO26:");
+  M5.begin(); //Init M5Stack.  初始化M5Stack
+  M5.Power.begin(); //Init power  初始化电源模块
+  M5.lcd.setTextSize(2);  //Set the text size to 2.  设置文字大小为2
+  M5.Lcd.printf("UNIT_LIGHT EXAMPLE\n\n");
+  M5.Lcd.println("Analog:");
+  M5.Lcd.println("Digital:");
+  pinMode(26, INPUT); //Set pin 26 as input mode.  设置引脚26为输入模式
 }
 
-uint16_t analogRead_value = 0;
-uint16_t digitalRead_value = 0;
 void loop() {
-  // put your main code here, to run repeatedly:
-  M5.Lcd.setCursor(175, 80);
-  M5.Lcd.setTextColor(BLACK);
+  static uint16_t digitalRead_value = 0, analogRead_value = 0;
+  analogRead_value = analogRead(36);  //Store the analog quantity read from pin 36.  将36号引脚读取到的模拟量存储
+  digitalRead_value = digitalRead(26);  //Store the number read from pin 26.  将26号引脚读取到的数字量存储
+  M5.Lcd.setCursor(90, 30);
   M5.Lcd.printf("%d\n", analogRead_value);
-  M5.Lcd.setCursor(175, 100);
-  M5.Lcd.printf("%d\n", digitalRead_value);
-  analogRead_value = analogRead(36);
-  digitalRead_value = digitalRead(26);
-  M5.Lcd.setTextColor(YELLOW);
-  M5.Lcd.setCursor(175, 80);
-  M5.Lcd.printf("%d\n", analogRead_value);
-  M5.Lcd.setCursor(175, 100);
+  M5.Lcd.setCursor(90, 50);
   M5.Lcd.printf("%d\n", digitalRead_value);
   delay(10);
 }
--- examples/Unit/RFID_RC522/RFID_RC522.ino
+++ examples/Unit/RFID_RC522/RFID_RC522.ino
@@ -1,69 +1,40 @@
 /*
-    Description: Use the RFID Unit to read the Fudan card ID and display the ID on the screen.
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with M5Core sample source code
+*                          配套  M5Core 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/core/gray
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/core/gray
+*
+* describe: RFID.  环境传感器
+* date：2021/8/17
+*******************************************************************************
+  Please connect to Port A(22、21),Use the RFID Unit to read the Fudan card ID and display the ID on the screen.
+  请连接端口A(22、21),使用RFID Unit 读取ID卡并在屏幕上显示。
 */
-#include <Wire.h>
-#include "MFRC522_I2C.h"
-#include <M5Stack.h>
 
-// 0x28 is i2c address on SDA. Check your address with i2cscanner if not match.
-MFRC522 mfrc522(0x28);   // Create MFRC522 instance.
+#include <M5Stack.h>
+#include "MFRC522_I2C.h"
+
+MFRC522 mfrc522(0x28);   // Create MFRC522 instance.  创建MFRC522实例
 
 void setup() {
-  M5.begin();
-  M5.Power.begin();
-  M5.Lcd.fillScreen( BLACK );
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.setTextColor(YELLOW);  
-  M5.Lcd.setTextSize(2);
-
-  M5.Lcd.fillScreen( BLACK );
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.println("M5StackFire MFRC522");
-  Serial.begin(115200);           // Initialize serial communications with the PC
-  Wire.begin();                   // Initialize I2C
-  
-  mfrc522.PCD_Init();             // Init MFRC522
-  ShowReaderDetails();            // Show details of PCD - MFRC522 Card Reader details
-  Serial.println(F("Scan PICC to see UID, type, and data blocks..."));
+  M5.begin(); //Init M5Stack.  初始化M5Stack
+  M5.Power.begin(); //Init power  初始化电源模块
+  M5.lcd.setTextSize(2);  //Set the text size to 2.  设置文字大小为2
+  M5.Lcd.println("MFRC522 Test");
+  Wire.begin(); //Wire init, adding the I2C bus.  Wire初始化, 加入i2c总线
+  mfrc522.PCD_Init(); // Init MFRC522.  初始化 MFRC522
   M5.Lcd.println("Scan PICC to see UID, type, and data blocks...");
 }
 
 void loop() {
-  // Look for new cards, and select one if present
-  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
-    delay(50);
+  if (!mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial()) {  //如果没有读取到新的卡片
     return;
   }
-  
-  // Now a card is selected. The UID and SAK is in mfrc522.uid.
-  
-  // Dump UID
-  Serial.print(F("Card UID:"));
-  M5.Lcd.println(" ");
   for (byte i = 0; i < mfrc522.uid.size; i++) {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
     M5.Lcd.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
     M5.Lcd.print(mfrc522.uid.uidByte[i], HEX);
-  } 
-  Serial.println();
-  
-}
-
-void ShowReaderDetails() {
-  // Get the MFRC522 software version
-  byte v = mfrc522.PCD_ReadRegister(mfrc522.VersionReg);
-  Serial.print(F("MFRC522 Software Version: 0x"));
-  Serial.print(v, HEX);
-  if (v == 0x91)
-    Serial.print(F(" = v1.0"));
-  else if (v == 0x92)
-    Serial.print(F(" = v2.0"));
-  else
-    Serial.print(F(" (unknown)"));
-  Serial.println("");
-  // When 0x00 or 0xFF is returned, communication probably failed
-  if ((v == 0x00) || (v == 0xFF)) {
-    Serial.println(F("WARNING: Communication failure, is the MFRC522 properly connected?"));
   }
+  M5.Lcd.println("");
 }
