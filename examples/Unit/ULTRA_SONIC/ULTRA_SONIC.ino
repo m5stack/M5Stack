259e6249dcddf8c0a0af32f7ffcea1d0fed26444
Tinyu <Tinyu.Zhao@gmail.com>
2021-08-19 17:55:36 +0800

    ULTRA_SONIC、Vibrator、MAKEY

--- examples/Unit/ULTRA_SONIC/ULTRA_SONIC.ino
+++ examples/Unit/ULTRA_SONIC/ULTRA_SONIC.ino
@@ -1,59 +1,48 @@
 /*
-    Description: Display the distance measured by ultrasonic
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with M5Core sample source code
+*                          配套  M5Core 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/core/gray
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/core/gray
+*
+* describe: Ultrasonic.  超声波测距传感器
+* date：2021/8/19
+*******************************************************************************
+  Please connect to Port A,Display the distance measured by ultrasonic
+  请连接端口A,显示超声波测量的距离
 */
 
 #include <M5Stack.h>
 
-TFT_eSprite Terminalbuff = TFT_eSprite(&M5.Lcd);
-
 void setup() {
-  // put your setup code here, to run once:
-    M5.begin();
-    
-    Wire.begin(21,22);
-    
-    Terminalbuff.createSprite(160, 80);
-    Terminalbuff.fillRect(80,20,160,80,BLACK);
-    
-    Terminalbuff.pushSprite(80,20);
-    Terminalbuff.setTextFont(4);
-    M5.Lcd.setCursor(105, 0, 4);
-    
-    M5.Lcd.print("Ultrasonic");
+  M5.begin(); //Init M5Stack.  初始化M5Stack
+  M5.Power.begin(); //Init power  初始化电源模块
+  Wire.begin(21, 22);
+  M5.Lcd.setCursor(105, 0, 4);  //Set the cursor at (105,0) and set the font to a 4 point font.  将光标设置在(105,0)处,且设置字体为4号字体
+  M5.Lcd.print("Ultrasonic\nDistance:");
 }
 
 float readEUS()
 {
-    uint32_t data;
-    Wire.beginTransmission(0x57);
-    Wire.write(0x01);
-    Wire.endTransmission();
-    delay(120);
-    Wire.requestFrom(0x57,3);
-    data  = Wire.read();data <<= 8;
-    data |= Wire.read();data <<= 8;
-    data |= Wire.read();
-    return float(data) / 1000;
-    
+  uint32_t data;
+  Wire.beginTransmission(0x57); //Transfer data to 0x57.  将数据传输到0x57
+  Wire.write(0x01);
+  Wire.endTransmission(); //Stop data transmission with the Ultrasonic Unit.  停止与Ultrasonic Unit的数据传输
+  delay(120);
+  Wire.requestFrom(0x57,3); //Request 3 bytes from Ultrasonic Unit.  向Ultrasonic Unit请求3个字节。
+  data  = Wire.read();data <<= 8;
+  data |= Wire.read();data <<= 8;
+  data |= Wire.read();
+  return float(data) / 1000;
 }
 
 void loop() {
-                                                                            
-    float newvalue = 0;
-
-    while(1)
-    {
-        newvalue = readEUS();
-
-        Terminalbuff.fillRect(80,20,160,80,BLACK);
-        Terminalbuff.setCursor(30,50);
-
-        if(( newvalue < 1500 )&&( newvalue > 20 ))
-        {
-            Terminalbuff.printf("%.2fmm",newvalue);
-            Terminalbuff.pushSprite(80,20);
-        }
-        delay(100);
-    }
-
+  static float newvalue = 0;
+  newvalue = readEUS();
+  if(( newvalue < 1500 )&&( newvalue > 20 )){
+    M5.Lcd.setCursor(105,27);
+    M5.Lcd.printf("%.2fmm",newvalue);
+  }
+  delay(100);
 }
