b0ca2180a9233f5aa2f32d13a324449265977d04
Tinyu <Tinyu.Zhao@gmail.com>
2021-08-30 10:28:55 +0800

    extio

--- examples/Unit/EXT_IO_PCA9554PW/EXT_IO_PCA9554PW.ino
+++ examples/Unit/EXT_IO_PCA9554PW/EXT_IO_PCA9554PW.ino
@@ -1,10 +1,21 @@
 /*
-    Description: Control the 8 extended IOs on the EXT.IO Unit to cycle high and low level changes.
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with M5Core sample source code
+*                          配套  M5Core 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/unit/extio
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/unit/extio
+*
+* describe: extio.
+* date：2021/8/30
+*******************************************************************************
+  Please connect to Port A,Control the 8 extended IOs on the EXT.IO Unit to cycle high and low level changes.
+  请连接端口 A,在EXT.IO Unit上控制8个扩展的IO来循环高电平和低电平的变化。
 */
 #include <M5Stack.h>
-#include "PCA9554.h"  // Load the PCA9554 Library
+#include "PCA9554.h"
 
-PCA9554 ioCon1(0x27);  // Create an object at this address
+PCA9554 ioCon1(0x27);  // Create an object at this address.  在这个地址上创建一个对象
 
 uint8_t res;
 void setup()
@@ -12,56 +23,38 @@ void setup()
   M5.begin();
   M5.Power.begin();
   Wire.begin();
-  M5.Lcd.clear(BLACK);
   M5.Lcd.setTextSize(2);
   M5.Lcd.setTextColor(YELLOW);
-  M5.Lcd.setCursor(70, 90);
+  M5.Lcd.setCursor(0, 90);
   M5.Lcd.print("UNIT_IO EXAMPLE\n");
-  M5.Lcd.setCursor(15, 130);
-  M5.Lcd.print("Connect Uint_io In PortA");
 
-  ioCon1.twiWrite(21, 22);
+  ioCon1.twiWrite(21, 22);  //Sets the I2C pin of the connection.  设置连接的I2C引脚
   delay(10);
   res = 1;
   ioCon1.twiRead(res);
   Serial.printf("res:%d\r\n", res);
 
-  ioCon1.portMode0(ALLOUTPUT); //Set the port as all output 
+  ioCon1.portMode(ALLOUTPUT); //Set the port as all output.  设置所有引脚为输出模式
 }
 
 void loop()
 {
-  // write single, the same read
-  ioCon1.digitalWrite0(0, LOW);
-  ioCon1.digitalWrite0(1, LOW);
-  ioCon1.digitalWrite0(2, LOW);
-  ioCon1.digitalWrite0(3, LOW);
-  ioCon1.digitalWrite0(4, LOW);
-  ioCon1.digitalWrite0(5, LOW);
-  ioCon1.digitalWrite0(6, LOW);
-  ioCon1.digitalWrite0(7, LOW);
+  for(int i=0;i<8;i++)  ioCon1.digitalWrite(i, LOW);
   delay(1000);
-  ioCon1.digitalWrite0(0, HIGH);
-  ioCon1.digitalWrite0(1, HIGH);
-  ioCon1.digitalWrite0(2, HIGH);
-  ioCon1.digitalWrite0(3, HIGH);
-  ioCon1.digitalWrite0(4, HIGH);
-  ioCon1.digitalWrite0(5, HIGH);
-  ioCon1.digitalWrite0(6, HIGH);
-  ioCon1.digitalWrite0(7, HIGH);
+  for(int i=0;i<8;i++)  ioCon1.digitalWrite(i, HIGH);
   delay(1000);
 
-  // write 0-7 HIGHT
-  Serial.println(ioCon1.digitalWritePort0(0xff));
+  // write 0-7 HIGH.  设置0~7号引脚为高电平
+  Serial.println(ioCon1.digitalWritePort(0xff));
   delay(200);
 
-  // write 0-7 LOW
-  Serial.println(ioCon1.digitalWritePort0(0x00));
+  // write 0-7 LOW.  设置0~7号引脚为低电平
+  Serial.println(ioCon1.digitalWritePort(0x00));
   delay(200);
 
-  //// write Port, the same read
+  // write Port, the same read
   for (byte i = 0; i < 8; i++) {
-    ioCon1.digitalWritePort0((1 << i));
+    ioCon1.digitalWritePort((1 << i));
     delay(200);
   }
 }
