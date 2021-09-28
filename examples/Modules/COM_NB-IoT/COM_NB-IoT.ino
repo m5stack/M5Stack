e33d2b3616e2ddcf121ac218c33b3d51caeef339
Tinyu <Tinyu.Zhao@gmail.com>
2021-09-28 13:33:01 +0800

    Update COM_NB-IoT.ino

--- examples/Modules/COM_NB-IoT/COM_NB-IoT.ino
+++ examples/Modules/COM_NB-IoT/COM_NB-IoT.ino
@@ -311,7 +311,6 @@ void setup()
 	M5.begin();
 	Serial.begin(115200);
 	Serial2.begin(115200, SERIAL_8N1, 5, 13);
-	//Serial.printf(" STC\n");
 
 	Disbuff.createSprite(320,20);
 	Disbuff.fillRect(0,0,320,20,BLACK);
