ec621695f2afbdfe5e9f98e75991e0af122540a4
Tinyu <Tinyu.Zhao@gmail.com>
2021-10-28 19:21:29 +0800

    Finger Unit

--- examples/Unit/FINGER_FPC1020A/FINGER_FPC1020A.ino
+++ examples/Unit/FINGER_FPC1020A/FINGER_FPC1020A.ino
@@ -1,17 +1,19 @@
-// /*
-// *******************************************************************************
-// * Copyright (c) 2021 by M5Stack
-// *                  Equipped with M5Tough sample source code
-// *                          配套  M5Tough 示例源代码
-// * Visit the website for more information：https://docs.m5stack.com/en/core/tough
-// * 获取更多资料请访问：https://docs.m5stack.com/zh_CN/core/tough
-// *
-// * describe：Hello World
-// * date：2021/7/21
-// *******************************************************************************
+/*
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with M5Core sample source code
+*                          配套  M5Core 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/core/gray
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/core/gray
+*
+* describe：Finger Unit example
+* date：2021/10/28
+*******************************************************************************
+Description: FINGER UNIT use case: Press the left button to enter the fingerprint entry mode. Press the middle button to enter the fingerprint identification mode，Right click to delete all saved users
+FINGER UNIT 使用案例按左键进入指纹录入模式，短按中间键进入指纹识别模式，按下右键删除所有保存的用户
+*/
 
 #include <M5Stack.h>
-
 #include "M5_FPC1020A.h"
 
 FingerPrint FP_M;
@@ -37,8 +39,6 @@ void loop(){
   uint8_t res1;
   //ButtonA: Add user.  添加用户
   if(M5.BtnA.wasPressed()){
-    
-  
     M5.Lcd.fillRect(0, 0, 320, 200, BLACK);
     Serial.println("Start Fingerprint Typing");
     Serial.println("Put Your Finger on the sensor");
@@ -48,7 +48,7 @@ void loop(){
     M5.Lcd.println("Put Your Finger on the sensor");
     M5.Lcd.println("wating....");
 
-    res1 = FP_M.fpm_addUser(22,1);//(user_num, userPermission)
+    res1 = FP_M.fpm_addUser(22,1);  //(user_num, userPermission)
     if(res1 == ACK_SUCCESS){
       M5.Lcd.println("Success");
       Serial.println("Success");
