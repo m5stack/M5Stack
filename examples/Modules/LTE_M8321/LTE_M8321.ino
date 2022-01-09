f3f20483d600e53e68955fc5983612949877ee5b
Tinyu <Tinyu.Zhao@gmail.com>
2022-01-09 15:40:19 +0800

    Update LTE_M8321.ino

--- examples/Modules/LTE_M8321/LTE_M8321.ino
+++ examples/Modules/LTE_M8321/LTE_M8321.ino
@@ -304,7 +304,6 @@ void setup()
     // put your setup code here, to run once:
     M5.begin();
     M5.Power.begin();
-    Serial.begin(115200);
     Serial2.begin(115200, SERIAL_8N1, 16, 17);
 
     Disbuff.createSprite(320, 100);
