607f91743095d26b3a2f0e477eabdf7a1a1ecd9b
Tinyu <Tinyu.Zhao@gmail.com>
2022-05-21 11:18:43 +0800

    add rs485 example

--- examples/Basics/Button/Button.ino
+++ examples/Basics/Button/Button.ino
@@ -12,19 +12,24 @@
 */
 #include <M5Stack.h>
 /* After M5Core is started or reset
-  the program in the setUp () function will be run, and this part will only be run once.
-  在 M5Core 启动或者复位后，即会开始执行setup()函数中的程序，该部分只会执行一次。 */
+  the program in the setUp () function will be run, and this part will only be
+  run once. 在 M5Core
+  启动或者复位后，即会开始执行setup()函数中的程序，该部分只会执行一次。 */
 void setup() {
-  M5.begin();  //Init M5Core.  初始化 M5Core
-  M5.Power.begin(); //Init Power module.  初始化电源模块
-  M5.Lcd.setTextColor(YELLOW);  //Set the font color to yellow.  设置字体颜色为黄色
-  M5.Lcd.setTextSize(2);  //Set the font size.  设置字体大小为2
-  M5.Lcd.setCursor(65, 10); //Move the cursor position to (65, 10).  移动光标位置到 (65, 10) 处
-  M5.Lcd.println("Button example"); //The screen prints the formatted string and wraps the line.  输出格式化字符串并换行
-  M5.Lcd.setCursor(3, 35);
-  M5.Lcd.println("Press button B for 700ms");
-  M5.Lcd.println("to clear screen.");
-  M5.Lcd.setTextColor(RED);
+    M5.begin();        // Init M5Core.  初始化 M5Core
+    M5.Power.begin();  // Init Power module.  初始化电源模块
+    M5.Lcd.setTextColor(
+        YELLOW);  // Set the font color to yellow.  设置字体颜色为黄色
+    M5.Lcd.setTextSize(2);     // Set the font size.  设置字体大小为2
+    M5.Lcd.setCursor(65, 10);  // Move the cursor position to (65, 10).
+                               // 移动光标位置到 (65, 10) 处
+    M5.Lcd.println(
+        "Button example");  // The screen prints the formatted string and wraps
+                            // the line.  输出格式化字符串并换行
+    M5.Lcd.setCursor(3, 35);
+    M5.Lcd.println("Press button B for 700ms");
+    M5.Lcd.println("to clear screen.");
+    M5.Lcd.setTextColor(RED);
 }
 
 /* After the program in setup() runs, it runs the program in loop()
@@ -32,15 +37,16 @@ The loop() function is an infinite loop in which the program runs repeatedly
 在setup()函数中的程序执行完后，会接着执行loop()函数中的程序
 loop()函数是一个死循环，其中的程序会不断的重复运行 */
 void loop() {
-  M5.update(); //Read the press state of the key.  读取按键 A, B, C 的状态
-  if (M5.BtnA.wasReleased() || M5.BtnA.pressedFor(1000, 200)) {
-    M5.Lcd.print('A');
-  } else if (M5.BtnB.wasReleased() || M5.BtnB.pressedFor(1000, 200)) {
-    M5.Lcd.print('B');
-  } else if (M5.BtnC.wasReleased() || M5.BtnC.pressedFor(1000, 200)) {
-    M5.Lcd.print('C');
-  } else if (M5.BtnB.wasReleasefor(700)) {
-    M5.Lcd.clear(WHITE);  // Clear the screen and set white to the background color.  清空屏幕并将白色设置为底色
-    M5.Lcd.setCursor(0, 0);
-  }
+    M5.update();  // Read the press state of the key.  读取按键 A, B, C 的状态
+    if (M5.BtnA.wasReleased() || M5.BtnA.pressedFor(1000, 200)) {
+        M5.Lcd.print('A');
+    } else if (M5.BtnB.wasReleased() || M5.BtnB.pressedFor(1000, 200)) {
+        M5.Lcd.print('B');
+    } else if (M5.BtnC.wasReleased() || M5.BtnC.pressedFor(1000, 200)) {
+        M5.Lcd.print('C');
+    } else if (M5.BtnB.wasReleasefor(700)) {
+        M5.Lcd.clear(WHITE);  // Clear the screen and set white to the
+                              // background color.  清空屏幕并将白色设置为底色
+        M5.Lcd.setCursor(0, 0);
+    }
 }
---
+++ examples/Modules/Base_PoE/LAN_W5500/WebServer/WebServer.ino
@@ -0,0 +1,133 @@
+/*
+    Description: Start the Web Server in the local network and the user can obtain the page response by accessing the assigned IP address.
+    Please install library before compiling:  
+    Ethernet2: file in M5stack lib examples -> modules -> W5500 -> Ethernet2.zip （unzip the lib zip file to the Arduino Lib path）
+*/
+#include <M5Stack.h>
+#include <SPI.h>
+#include <Ethernet2.h>
+#define SCK 18
+#define MISO 19
+#define MOSI 23
+#define CS 26
+
+//  01 05 00 01 02 00 9d 6a
+char uart_buffer[8] = {0x01, 0x05, 0x00, 0x01, 0x02, 0x00, 0x9d, 0x6a};
+char uart_rx_buffer[8] = {0};
+
+char Num = 0;
+char stringnum = 0;
+unsigned long W5500DataNum = 0;
+unsigned long Send_Num_Ok = 0;
+unsigned long Rec_Num = 0;
+unsigned long Rec_Num_Ok = 0;
+
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192, 168, 1, 177);
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 80 is default for HTTP):
+EthernetServer server(80);
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  M5.begin(true, false, true);
+  M5.Power.begin();
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  SPI.begin(SCK, MISO, MOSI, -1);
+  Ethernet.init(CS);
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+
+  M5.Lcd.println("M5Stack W5500 Test");
+  M5.Lcd.println(" ");
+  M5.Lcd.print(Ethernet.localIP());
+}
+
+
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+
+          client.println("<body>");
+          client.println("<h1>M5Stack W5500 Test</h1>");
+          client.println("<br />");
+          client.println("<p>Please click here</p>");
+          client.println("<a href=\"http://www.M5Stack.com\">M5Stack</a>");
+          client.println("<br />");
+          client.println("<br />");
+          client.println("<br />");
+
+          
+          client.print("W5500 Counter Num :");
+          client.print(W5500DataNum);
+          client.println("<br />");
+          client.println("<br />");
+          W5500DataNum ++;
+          
+          client.print("Rec_Num_Ok Counter :");
+          client.print(Rec_Num_Ok);
+          client.println("<br />");
+          client.println("<br />");
+
+          client.print("Rec_Num Counter :");
+          client.print(Rec_Num);
+          client.println("<br />");
+          client.println("<br />");
+
+        
+        
+          client.println("</body>");
+
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
+
