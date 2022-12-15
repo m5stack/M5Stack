6ab55cfa20f5d8f31caee8d49e1db1b62c2eff37
TinyuChiu <TInyu.Zhao@gmail.com>
2022-12-15 10:46:29 +0800

    Add PM25 Kit example

---
+++ examples/KIT/PM25/DFRobot_SHT20.cpp
@@ -0,0 +1,114 @@
+#include "DFRobot_SHT20.h"
+
+void DFRobot_SHT20::initSHT20(TwoWire &wirePort)
+{
+    i2cPort = &wirePort;
+    i2cPort->begin();
+}
+
+uint16_t DFRobot_SHT20::readValue(byte cmd)
+{
+    i2cPort->beginTransmission(SLAVE_ADDRESS);
+    i2cPort->write(cmd);
+    i2cPort->endTransmission();
+    byte toRead;
+    byte counter;
+    for(counter = 0, toRead = 0 ; counter < MAX_COUNTER && toRead != 3; counter++){
+        delay(DELAY_INTERVAL);
+        toRead = i2cPort->requestFrom(SLAVE_ADDRESS, 3);
+    }
+    if(counter == MAX_COUNTER){
+        return (ERROR_I2C_TIMEOUT);
+    }
+    byte msb, lsb, checksum;
+    msb = i2cPort->read();
+    lsb = i2cPort->read();
+    checksum = i2cPort->read();
+    uint16_t rawValue = ((uint16_t) msb << 8) | (uint16_t) lsb;
+    if(checkCRC(rawValue, checksum) != 0){
+        return (ERROR_BAD_CRC);
+    }
+    return rawValue & 0xFFFC;
+}
+
+float DFRobot_SHT20::readHumidity(void)
+{
+    uint16_t rawHumidity = readValue(TRIGGER_HUMD_MEASURE_NOHOLD);
+    if(rawHumidity == ERROR_I2C_TIMEOUT || rawHumidity == ERROR_BAD_CRC){
+        return(rawHumidity);
+    }
+    float tempRH = rawHumidity * (125.0 / 65536.0);
+    float rh = tempRH - 6.0;
+    return (rh);
+}
+
+float DFRobot_SHT20::readTemperature(void)
+{
+    uint16_t rawTemperature = readValue(TRIGGER_TEMP_MEASURE_NOHOLD);
+    if(rawTemperature == ERROR_I2C_TIMEOUT || rawTemperature == ERROR_BAD_CRC){
+        return(rawTemperature);
+    }
+    float tempTemperature = rawTemperature * (175.72 / 65536.0);
+    float realTemperature = tempTemperature - 46.85;
+    return (realTemperature);
+}
+
+void DFRobot_SHT20::setResolution(byte resolution)
+{
+    byte userRegister = readUserRegister();
+    userRegister &= B01111110;
+    resolution &= B10000001;
+    userRegister |= resolution;
+    writeUserRegister(userRegister);
+}
+
+byte DFRobot_SHT20::readUserRegister(void)
+{
+    byte userRegister;
+    i2cPort->beginTransmission(SLAVE_ADDRESS);
+    i2cPort->write(READ_USER_REG);
+    i2cPort->endTransmission();
+    i2cPort->requestFrom(SLAVE_ADDRESS, 1);
+    userRegister = i2cPort->read();
+    return (userRegister);
+}
+
+void DFRobot_SHT20::writeUserRegister(byte val)
+{
+    i2cPort->beginTransmission(SLAVE_ADDRESS);
+    i2cPort->write(WRITE_USER_REG);
+    i2cPort->write(val);
+    i2cPort->endTransmission();
+}
+
+byte DFRobot_SHT20::checkCRC(uint16_t message_from_sensor, uint8_t check_value_from_sensor)
+{
+    uint32_t remainder = (uint32_t)message_from_sensor << 8;
+    remainder |= check_value_from_sensor;
+    uint32_t divsor = (uint32_t)SHIFTED_DIVISOR;
+    for(int i = 0 ; i < 16 ; i++){
+        if(remainder & (uint32_t)1 << (23 - i)){
+            remainder ^= divsor;
+        }
+        divsor >>= 1;
+    }
+    return (byte)remainder;
+}
+
+void DFRobot_SHT20::showReslut(const char *prefix, int val)
+{
+    Serial.print(prefix);
+    if(val){
+        Serial.println("yes");
+    }else{
+        Serial.println("no");
+    }
+}
+
+void DFRobot_SHT20::checkSHT20(void)
+{
+    byte reg = readUserRegister();
+    showReslut("End of battery: ", reg & USER_REGISTER_END_OF_BATTERY);
+    showReslut("Heater enabled: ", reg & USER_REGISTER_HEATER_ENABLED);
+    showReslut("Disable OTP reload: ", reg & USER_REGISTER_DISABLE_OTP_RELOAD);
+}
---
+++ examples/KIT/PM25/PM25.ino
@@ -0,0 +1,183 @@
+#include <M5Stack.h>
+#include "Free_Fonts.h"
+#include <Wire.h>
+#include "UNIT_ENV.h"
+
+SHT3X sht30;
+
+#define TFT_GREY 0x7BEF
+
+#define DATA_LEN 32
+
+#define X_LOCAL 40
+#define Y_LOCAL 30
+
+#define X_OFFSET 160
+#define Y_OFFSET 23
+
+uint16_t CheckSum;
+uint16_t CheckCode;
+
+// Print the header for a display screen
+void header(const char *string, uint16_t color) {
+    M5.Lcd.fillScreen(color);
+    M5.Lcd.setTextSize(1);
+    M5.Lcd.setTextColor(TFT_MAGENTA, TFT_BLUE);
+    M5.Lcd.fillRect(0, 0, 320, 30, TFT_BLUE);
+    M5.Lcd.setTextDatum(TC_DATUM);
+    M5.Lcd.drawString(string, 160, 3, 4);
+}
+void setup() {
+    M5.begin();
+    Serial.begin(9600);
+    Serial2.begin(9600, SERIAL_8N1, 16, 17);
+    pinMode(13, OUTPUT);
+    digitalWrite(13, 1);
+
+    M5.Lcd.fillScreen(TFT_BLACK);
+    header("P M 2.5", TFT_BLACK);
+}
+
+uint8_t Air_val[32] = {0};
+int16_t p_val[16]   = {0};
+uint8_t i           = 0;
+
+#define FRONT 2
+
+void LCD_Display_Val(void) {
+    for (int i = 0, j = 0; i < 32; i++) {
+        if (i % 2 == 0) {
+            p_val[j] = Air_val[i];
+            p_val[j] = p_val[j] << 8;
+        } else {
+            p_val[j] |= Air_val[i];
+            j++;
+        }
+    }
+
+    //     M5.Lcd.setTextSize(FRONT);
+    M5.Lcd.setTextColor(TFT_RED, TFT_BLACK);
+    M5.Lcd.setCursor(X_LOCAL, Y_LOCAL, FRONT);
+    M5.Lcd.print("S P M");
+
+    M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+
+    M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET, FRONT);
+    M5.Lcd.print("                     ");
+    M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET, FRONT);
+    M5.Lcd.print("PM1.0 : ");
+    M5.Lcd.print(p_val[2]);
+
+    M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * 2, FRONT);
+    M5.Lcd.print("                     ");
+    M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * 2, FRONT);
+    M5.Lcd.print("PM2.5 : ");
+    M5.Lcd.print(p_val[3]);
+
+    M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * 3, FRONT);
+    M5.Lcd.print("                     ");
+    M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * 3, FRONT);
+    M5.Lcd.print("PM10  : ");
+    M5.Lcd.print(p_val[4]);
+
+    M5.Lcd.setTextColor(TFT_RED, TFT_BLACK);
+    M5.Lcd.setCursor(X_LOCAL + X_OFFSET, Y_LOCAL, FRONT);
+    M5.Lcd.print("A T M E");
+
+    M5.Lcd.setCursor(X_LOCAL + X_OFFSET, Y_LOCAL + Y_OFFSET, FRONT);
+    M5.Lcd.print("                     ");
+    M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+    M5.Lcd.setCursor(X_LOCAL + X_OFFSET, Y_LOCAL + Y_OFFSET, FRONT);
+    M5.Lcd.print("PM1.0 : ");
+    M5.Lcd.print(p_val[5]);
+
+    M5.Lcd.setCursor(X_LOCAL + X_OFFSET, Y_LOCAL + Y_OFFSET * 2, FRONT);
+    M5.Lcd.print("                     ");
+    M5.Lcd.setCursor(X_LOCAL + X_OFFSET, Y_LOCAL + Y_OFFSET * 2, FRONT);
+    M5.Lcd.print("PM2.5 : ");
+    M5.Lcd.print(p_val[6]);
+
+    M5.Lcd.setCursor(X_LOCAL + X_OFFSET, Y_LOCAL + Y_OFFSET * 3, FRONT);
+    M5.Lcd.print("                     ");
+    M5.Lcd.setCursor(X_LOCAL + X_OFFSET, Y_LOCAL + Y_OFFSET * 3, FRONT);
+    M5.Lcd.print("PM10  : ");
+    M5.Lcd.print(p_val[7]);
+
+    M5.Lcd.setTextColor(TFT_RED, TFT_BLACK);
+    M5.Lcd.setCursor(X_LOCAL + X_OFFSET / 4, Y_LOCAL + Y_OFFSET * 4, FRONT);
+    M5.Lcd.print("Number of particles");
+
+    M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * 5, FRONT);
+    M5.Lcd.print("                     ");
+    M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+    M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * 5, FRONT);
+    M5.Lcd.print("0.3um : ");
+    M5.Lcd.print(p_val[8]);
+
+    M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * 6, FRONT);
+    M5.Lcd.print("                     ");
+    M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * 6, FRONT);
+    M5.Lcd.print("0.5um : ");
+    M5.Lcd.print(p_val[9]);
+
+    M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * 7, FRONT);
+    M5.Lcd.print("1.0um : ");
+    M5.Lcd.print(p_val[10]);
+
+    M5.Lcd.setCursor(X_LOCAL + X_OFFSET, Y_LOCAL + Y_OFFSET * 5, FRONT);
+    M5.Lcd.print("                     ");
+    M5.Lcd.setCursor(X_LOCAL + X_OFFSET, Y_LOCAL + Y_OFFSET * 5, FRONT);
+    M5.Lcd.print("2.5um : ");
+    M5.Lcd.print(p_val[11]);
+
+    M5.Lcd.setCursor(X_LOCAL + X_OFFSET, Y_LOCAL + Y_OFFSET * 6, FRONT);
+    M5.Lcd.print("                     ");
+    M5.Lcd.setCursor(X_LOCAL + X_OFFSET, Y_LOCAL + Y_OFFSET * 6, FRONT);
+    M5.Lcd.print("5.0um : ");
+    M5.Lcd.print(p_val[12]);
+
+    M5.Lcd.setCursor(X_LOCAL + X_OFFSET, Y_LOCAL + Y_OFFSET * 7, FRONT);
+    M5.Lcd.print("                     ");
+    M5.Lcd.setCursor(X_LOCAL + X_OFFSET, Y_LOCAL + Y_OFFSET * 7, FRONT);
+    M5.Lcd.print("10um  : ");
+    M5.Lcd.print(p_val[13]);
+}
+
+void TempHumRead(void) {
+    float humd, temp;
+    if (sht30.get() == 0) {  // Obtain the data of shT30.  获取sht30的数据
+        humd = sht30.cTemp;  // Read Humidity
+        temp = sht30.humidity;  // Read Temperature
+    } else {
+        humd = 0;  // Read Humidity
+        temp = 0;  // Read Temperature
+    }
+
+    M5.Lcd.setTextColor(TFT_GREEN, TFT_BLACK);
+    M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * 8, FRONT);
+    M5.Lcd.print("                     ");
+    M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * 8, FRONT);
+    M5.Lcd.print("T M P : ");
+    M5.Lcd.print(temp);
+
+    M5.Lcd.setCursor(X_LOCAL + X_OFFSET, Y_LOCAL + Y_OFFSET * 8, FRONT);
+    M5.Lcd.print("                     ");
+    M5.Lcd.setCursor(X_LOCAL + X_OFFSET, Y_LOCAL + Y_OFFSET * 8, FRONT);
+    M5.Lcd.print("H U M : ");
+    M5.Lcd.print(humd);
+}
+
+void loop() {
+    if (Serial2.available()) {
+        Air_val[i] = Serial2.read();
+        Serial.write(Air_val[i]);
+        i++;
+    } else {
+        i = 0;
+    }
+
+    if (i == DATA_LEN) {
+        LCD_Display_Val();
+        TempHumRead();
+    }
+}
