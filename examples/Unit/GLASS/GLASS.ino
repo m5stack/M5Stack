4d4c6565cd09f0bea1f2614a88d52910c7444c2b
TinyuChiu <TInyu.Zhao@gmail.com>
2023-03-12 22:44:58 +0800

    Fix GLASS can't display full characters

--- examples/Unit/GLASS/GLASS.ino
+++ examples/Unit/GLASS/GLASS.ino
@@ -3,12 +3,15 @@
 * Copyright (c) 2023 by M5Stack
 *                  Equipped with M5Core sample source code
 *                          配套  M5Core 示例源代码
-* Visit for more information: https://docs.m5stack.com/en/core/gray
-* 获取更多资料请访问: https://docs.m5stack.com/zh_CN/core/gray
+* Visit for more information: https://docs.m5stack.com/en/unit/Glass%20Unit
+* 获取更多资料请访问: https://docs.m5stack.com/zh_CN/unit/Glass%20Unit
 *
 * Describe: Glass.
 * Date: 2023/2/23
 *******************************************************************************
+Connect the Unit_Glass to Port A and press the buttons on the left/right side of
+the Unit Glass to subtract/add to the counter.
+将Unit_Glass连接到Port A,按Unit Glass左/右侧的按键来对计数器进行减/加.
 */
 #include <M5Stack.h>
 #include <UNIT_GLASS.h>
@@ -34,9 +37,10 @@ void setup() {
     string_buffer = "M5STACK";
     Glass.set_string_buffer(string_buffer.c_str());
     Glass.draw_string(36, 0, 16, 1);
-    string_buffer = "   2023-02-24";
+    delay(20);  // Ensure complete character display. 保证字符显示完全
+    string_buffer = "2023-02-24";
     Glass.set_string_buffer(string_buffer.c_str());
-    Glass.draw_string(0, 24, 16, 1);
+    Glass.draw_string(24, 24, 16, 1);
     Glass.show();
 }
 
@@ -57,10 +61,11 @@ void loop() {
         count--;
     }
 
-    string_buffer = "   Count: ";
+    string_buffer = "Count: ";
     string_buffer += count;
     string_buffer += "      ";
     Glass.set_string_buffer(string_buffer.c_str());
-    Glass.draw_string(0, 48, 16, 1);
+    Glass.draw_string(36, 48, 16, 1);
     Glass.show();
+    delay(20);
 }
