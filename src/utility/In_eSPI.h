74a76812de79ed7d0264d94aa08aae7d5527da00
ChloeGrt <98591686+ChloeGrt@users.noreply.github.com>
2024-06-21 11:02:47 +0200

    fix problem min function

--- src/utility/In_eSPI.h
+++ src/utility/In_eSPI.h
@@ -611,9 +611,9 @@ static inline void swap_coord(T &a, T &b) {
     b   = t;
 }
 
-#ifndef min
+#ifndef minimum
 // Return minimum of two numbers, may already be defined
-#define min(a, b) (((a) < (b)) ? (a) : (b))
+#define minimum(a, b) (((a) < (b)) ? (a) : (b))
 #endif
 
 // This structure allows sketches to retrieve the user setup parameters at
